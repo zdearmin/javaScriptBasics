29f1470bb6d77b4ada040a058b0f82da95319d0a
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 17:18:44 -0400

    03-numbers added

--- 03-numbers.js
+++ 03-numbers.js
@@ -18,3 +18,39 @@ let powerLevel = 9001;
 let southPoleTempF = -44;
 let spaceStationCrewCount = -7;
 const imNotANumber = NaN;
+
+// Arithmetic Operators
+1 + 1; // Addition
+
+12-10; // Subtraction
+
+125 * 1.07; // Multiplication
+
+125 / 4; // Division
+
+10 ** 2; // Exponentiation
+
+15 % 2; // Modulus
+
+// You would think this would be 0.3 right?
+console.log(0.1 + 0.2);
+
+// There's something fishy going on here?
+console.log(1.1 * 1.1);
+
+// ==== Mini-Challenge ====
+let billAmount = 132.96;
+let totalPeople = 5;
+const salesTax = 0.07;
+
+// Calculate the amount of tax on the bill
+let taxAmount = billAmount * salesTax;
+
+// Calculate the grand total
+let grandTotal = taxAmount + billAmount;
+
+// Calculate how much each person will pay
+let individualContribution = grandTotal / totalPeople;
+
+// Print the result to verify the procedure works
+console.log(individualContribution);
