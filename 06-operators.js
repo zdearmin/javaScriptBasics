3c200222e0c5941597c8df2352d611ae821546db
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 19:06:22 -0400

    06-operators added

---
+++ 06-operators.js
@@ -0,0 +1,86 @@
+console.log(5 + 5); // Addition operator (+)
+console.log(6 - 6); // Subtraction operator (-)
+console.log(7 * 7); // Multiplication operator (*)
+console.log(8 / 8); // Division operator (/)
+console.log(2 ** 3); // Exponentiation operator (**)
+
+// moduluo operator (%)console.log(10 % 5); // Result = 0
+console.log(10 % 4); // Result = 2
+console.log(10 % 9); // Result = 1
+
+10 / 5 = 2 with a remainder of 0.
+10 / 4 = 2 with a remainder of 2.
+10 / 9 = 1 with a remainder of 1.
+
+let x = 5;
+let y = 2;
+let x = 7;
+console.log(x, y, z); // displays 5 2 7
+x = y = z;
+console.log(x, y, z); // displays 7 7 7
+
+let numTwo = 2;
+let two = "2";
+console.log(numTwo == two) // returns true
+
+let numTwo = 2;
+let two = "2";
+console.log(numTwo === two) // returns false
+
+let name = "Paul";
+let age = 30;
+console.log(name != age) // returns true
+
+let age = 28;
+let strAge = "27";
+console.log(age !== strAge) // returns true
+
+let age = 27;
+let strAge = "27";
+console.log(age !== strAge) // returns true
+
+let count = 2;
+count = count + 1;
+console.log(count)
+
+let count = 2;
+count += 1;
+console.log(count) // += implies we want to add 1 to the count variable (e.g. a = a + b)
+// Also works for -=, *=, /=, %=
+
+let y = 5;
+y = y + 1; y += 1;
+
+let z = 10;
+console.log(z);
+z += 1;
+console.log(z);
+
+let a = 20;
+console.log(a);
+a -= 1;
+console.log(a);
+
+let b = 25;
+console.log(b);
+b *= 2;
+console.log(b);
+
+let c = 50;
+console.log(c);
+c /= 2;
+console.log(c);
+
+let age = 40;
+age += 1;
+age -= 20;
+age *= 20;
+console.log(age);
+
+console.log(2 > 1); // true
+console.log(3 < 2); // false
+
+var test = 2 >=3; // What will this print?
+console.log(test);
+console.log("Two is greater than one? " + (2 > 1));
+console.log(3 >= 1);
