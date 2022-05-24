26dd1cb009bc6907656c8591839a966567261096
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 22:42:18 -0400

    10-ifElse added

---
+++ 10-ifElse.js
@@ -0,0 +1,29 @@
+// 1
+if (condition) {
+    // code to be executed if the condition is true //2
+} else { //3
+    // code to be executed if the contition is false
+}
+
+// Example 1
+if (10 > 5) {
+    console.log("10 is greater than 5")
+} else {
+    console.log("10 is not greater than 5")
+}
+
+// Example 2
+if (10 < 5) {
+    console.log("10 is less than 5")
+} else {
+    console.log("10 is not less than 5")
+}
+
+// Example 3
+let weather = "sunny"
+
+if (weather === "rainy") {
+    console.log("Bring an umbrella")
+} else {
+    console.log("You don't need an umbrella")
+}
