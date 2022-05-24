c2a7507ed026b0647bde7ba08c653421ef48a205
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 22:34:32 -0400

    09-ifStatements added

---
+++ 09-ifStatements.js
@@ -0,0 +1,12 @@
+// // 1    // 2
+// if (condition) { //3
+// // block of code to be executed if the condition is true
+// };
+
+if (10 > 5) {
+    console.log("Yep, 10 is greater than 5");
+};
+
+if (10 < 5) {
+    console.log("This code will not run! 10 is not greater than 5.");
+}
