e8ca54bd57f5a13d88d64e685dfe04e3931fd08b
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 22:26:27 -0400

    08-arrays added

---
+++ 08-arrays.js
@@ -0,0 +1,20 @@
+let rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+let raceWinners = [33, 72, 64];
+let myFavoriteThings = ['Broccoli', 46204, 'Zombie Cats From Mars'];
+
+var mySecretSpyInfo = ["James Bond", 007, true]; // A string, a number (although the 00 is ignored), and a boolean
+
+var countryArray = ["USA", "Japan", "Belgium"];
+
+console.log(countryArray[2]); // What will print here? Belgium
+console.log(countryArray[0]); // What will pring here? USA
+
+let dogs = ["Shibu Inu", "Husky", "Corgi", "Great Dane"];
+let cats = ["Maine Coon", "Bengal", "Siamese", "Sphynx"];
+let birds = ["African Grey Parrot", "Canary", "Macaw", "Finch"];
+let fish = ["Goldfish", "Neon Tetra", "Betta", "Guppy"];
+
+console.log(dogs[0]);
+console.log(cats[1]);
+console.log(fish[3]);
+console.log(birds[0]);
