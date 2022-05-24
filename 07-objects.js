bca70339f96d77bd90f03f8399160dba73c7a333
Zach Dearmin <zacharydearmin@gmail.com>
2022-05-23 22:04:03 -0400

    07-objects added

---
+++ 07-objects.js
@@ -0,0 +1,44 @@
+ //1        //2
+ let zelda = {
+     //3            //4
+     name           : "Zelda",
+     age            : 17,
+     title          : "Princess",
+     isCaptured     : true
+ }
+
+ console.log(zelda); //1
+ console.log(zelda.name); //2
+ console.log(zelda.age); //3
+
+ console.log(zelda.middleName); // undefined: middleName does not existk
+
+ let friend = {
+     name: "Bob",
+     age: 50,
+     location: "California",
+     hairColor: "Black"
+ }
+
+ let movie = {
+    name: "Avengers", 
+    releaseDate: 2012,
+     director: "Joss Whedon",
+     cast: "Robert Downey Jr."
+ }
+
+ let favoriteTeam = {
+     state: "Tennessee",
+     college: "University of Tennessee",
+     sport: "Football and basketball"
+ }
+
+ console.log(friend);
+ console.log(friend.age);
+
+ console.log(movie);
+ console.log(movie.name);
+ console.log(movie.director);
+
+ console.log(favoriteTeam)
+ console.log(favoriteTeam.college)
